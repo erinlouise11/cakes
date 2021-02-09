5b56d5847cab4fb75b4ce248ca75a9f1fb0fafe7
Erin Murphy <erinlouise11@gmail.com>
2021-02-09 18:20:42 -0500

    Adding content to homepage

--- src/pages.js
+++ src/pages.js
@@ -3,40 +3,49 @@ import './App.css';
 import * as s from './styles';
 import {Link} from 'react-router-dom';
 import logo from './img/logo.png';
+import lady from './img/ladySmile.jpg';
 
 export function Home() {
     return (
         <section>
-            <s.HeroDivBig className="home-hero hero">
+            <s.HeroDivBig className="home-hero bg">
                 <s.homeHeroFlex className="hero-intro-div">
                     <s.H1>yummm...</s.H1>
                     <s.largerP>Our delicious products are hand crafted and home baked. Shipped with love all over the US.</s.largerP>
-                    <div className="button-container">
-                        <button className="order-button">ORDER NOW</button>
-                        <button className="products-button">SEE PRODUCTS</button>
-                    </div>                    
+                    <s.buttonDiv className="button-container">
+                        <s.pinkButton className="order-button">ORDER NOW</s.pinkButton>
+                        <s.darkButton className="products-button">SEE PRODUCTS</s.darkButton>
+                    </s.buttonDiv>                    
                 </s.homeHeroFlex>
             </s.HeroDivBig>           
             <s.Nav>
                 <s.Ul>
                     <Link className="nav-link" to="/products"><s.Li>Products</s.Li></Link>
                     <Link className="nav-link" to="/occasions"><s.Li>Occasions</s.Li></Link>
-                    <Link className="nav-link" to="/"><s.Li><s.Img src={logo} /></s.Li></Link>
+                    <Link className="nav-link" to="/"><s.Li><s.Logo src={logo} /></s.Li></Link>
                     <Link className="nav-link" to="/about"><s.Li>Our Story</s.Li></Link>
                     <Link className="nav-link" to="/order"><s.Li>Order</s.Li></Link>
                 </s.Ul>
             </s.Nav>
 
-            <div className="bio-div">
+            <s.whiteDiv className="bio-div white">
+                <s.divImg src={lady} />
+                <s.divInfo>
+                    <s.H2>the short intro</s.H2>
+                    <s.smallerP>Since a love for baking arose from a high school bake sale, the art of cakes and cupcakes have been what I live for. I can't wait for you to experience some Cool AF Cakes!</s.smallerP>
+                    <s.darkButton>LEARN MORE</s.darkButton>
+                </s.divInfo>
+            </s.whiteDiv>
+
+            <div className="products-div dark">
 
             </div>
-            <div className="products-div">
 
-            </div>
-            <div className="occasions-div">
+            <s.whiteDiv className="occasions-div white">
 
-            </div>
-            <div className="reviews-div">
+            </s.whiteDiv>
+            
+            <div className="reviews-div dark">
 
             </div>
         </section>       
@@ -46,12 +55,12 @@ export function Home() {
 export function Products() {
     return (
         <section>
-            <s.HeroDivSmall className="products-hero hero" /> 
+            <s.HeroDivSmall className="products-hero bg" /> 
             <s.Nav>
                 <s.Ul>
                     <Link className="nav-link" to="/products"><s.Li>Products</s.Li></Link>
                     <Link className="nav-link" to="/occasions"><s.Li>Occasions</s.Li></Link>
-                    <Link className="nav-link" to="/"><s.Li><s.Img src={logo} /></s.Li></Link>
+                    <Link className="nav-link" to="/"><s.Li><s.Logo src={logo} /></s.Li></Link>
                     <Link className="nav-link" to="/about"><s.Li>Our Story</s.Li></Link>
                     <Link className="nav-link" to="/order"><s.Li>Order</s.Li></Link>
                 </s.Ul>
@@ -63,12 +72,12 @@ export function Products() {
 export function Occasions() {
     return (
         <section>
-            <s.HeroDivSmall className="occasions-hero hero" /> 
+            <s.HeroDivSmall className="occasions-hero bg" /> 
             <s.Nav>
                 <s.Ul>
                     <Link className="nav-link" to="/products"><s.Li>Products</s.Li></Link>
                     <Link className="nav-link" to="/occasions"><s.Li>Occasions</s.Li></Link>
-                    <Link className="nav-link" to="/"><s.Li><s.Img src={logo} /></s.Li></Link>
+                    <Link className="nav-link" to="/"><s.Li><s.Logo src={logo} /></s.Li></Link>
                     <Link className="nav-link" to="/about"><s.Li>Our Story</s.Li></Link>
                     <Link className="nav-link" to="/order"><s.Li>Order</s.Li></Link>
                 </s.Ul>
@@ -80,12 +89,12 @@ export function Occasions() {
 export function About() {
     return (
         <section>
-        <s.HeroDivSmall className="about-hero hero" /> 
+        <s.HeroDivSmall className="about-hero bg" /> 
         <s.Nav>
             <s.Ul>
                 <Link className="nav-link" to="/products"><s.Li>Products</s.Li></Link>
                 <Link className="nav-link" to="/occasions"><s.Li>Occasions</s.Li></Link>
-                <Link className="nav-link" to="/"><s.Li><s.Img src={logo} /></s.Li></Link>
+                <Link className="nav-link" to="/"><s.Li><s.Logo src={logo} /></s.Li></Link>
                 <Link className="nav-link" to="/about"><s.Li>Our Story</s.Li></Link>
                 <Link className="nav-link" to="/order"><s.Li>Order</s.Li></Link>
             </s.Ul>
@@ -97,12 +106,12 @@ export function About() {
 export function Order() {
     return (
         <section>
-        <s.HeroDivSmall className="order-hero hero" /> 
+        <s.HeroDivSmall className="order-hero bg" /> 
         <s.Nav>
             <s.Ul>
                 <Link className="nav-link" to="/products"><s.Li>Products</s.Li></Link>
                 <Link className="nav-link" to="/occasions"><s.Li>Occasions</s.Li></Link>
-                <Link className="nav-link" to="/"><s.Li><s.Img src={logo} /></s.Li></Link>
+                <Link className="nav-link" to="/"><s.Li><s.Logo src={logo} /></s.Li></Link>
                 <Link className="nav-link" to="/about"><s.Li>Our Story</s.Li></Link>
                 <Link className="nav-link" to="/order"><s.Li>Order</s.Li></Link>
             </s.Ul>
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,96 @@
 import styled from 'styled-components';
 // import homeHeroImg from './img/homeBG.jpg';
 
+export const pinkButton = styled.button`
+    width: auto;
+    height: 60px;
+    margin-top: 20px;
+    padding: 10px 20px;
+    background-color: #CC84BD;
+    color: #222;
+    border: solid 5px #222;
+    font-size: 20px;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    letter-spacing: 1px;
+`;
+
+export const darkButton = styled.button`
+    width: auto;
+    height: 60px;
+    margin-top: 20px;
+    padding: 10px 20px;
+    background-color: #222;
+    color: #CC84BD;
+    border: solid 5px #222;
+    font-size: 20px;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    letter-spacing: 1px;
+`;
+
+export const H1 = styled.h1`
+    margin: 0;
+    font-size: 80px;
+    font-weight: bold;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    letter-spacing: 2px;
+    color: #222;
+`;
+
+export const largerP = styled.p`
+    margin: 0;
+    font-size: 22px;
+    font-family: sans-serif;
+    color: #222;
+`;
+
+export const smallerP = styled.p`
+    width: 500px;
+    margin: 0;
+    font-size: 18px;
+    font-family: sans-serif;
+    color: #222;
+`;
+
+export const H2 = styled.h2`
+    margin: 0;
+    font-size: 50px;
+    font-weight: bold;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    letter-spacing: 2px;
+    color: #222;
+`;
+
+export const Ul = styled.ul`
+    width: 1000px;
+    height: 110px;
+    padding-left: 0;
+    margin: 0;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;    
+`;
+
+export const Li = styled.li`
+    display: inline;
+    font-size: 22px;
+    font-family: sans-serif;
+    color: #222;
+`;
+
+export const Logo = styled.img`
+    width: 100px;
+    height: 100px;
+`;
+
+export const divImg = styled.img`
+    width: 1000px;
+    height: 500px;    
+    object-fit: cover;
+    object-position: top;
+`;
+
 export const HeroDivBig = styled.div`
     & {
         width: 100%;
@@ -14,7 +104,7 @@ export const HeroDivBig = styled.div`
         display: block;
         position: absolute;
         background-color: #111;
-        opacity: 0.3;
+        opacity: 0.1;
         width: 100%;
         height: 100%;
     }
@@ -32,8 +122,8 @@ export const HeroDivSmall = styled.div`
         content: '';
         display: block;
         position: absolute;
-        background-color: #666;
-        opacity: 0.2;
+        background-color: #fff;
+        opacity: 0.1;
         width: 100%;
         height: 100%;
     }
@@ -55,7 +145,7 @@ export const homeHeroFlex = styled.div`
     height: 260px;
     position: absolute;
     z-index: 1;
-    top: 20%;
+    bottom: 20%;
     left: 150px;
     display: flex;
     flex-direction: column;
@@ -63,59 +153,30 @@ export const homeHeroFlex = styled.div`
     justify-content: space-evenly;      
 `;
 
-export const Ul = styled.ul`
-    width: 1000px;
-    height: 110px;
-    padding-left: 0;
-    margin: 0;
-    list-style: none;
+export const buttonDiv = styled.div` 
+    width: 350px;
+    height: 60px;
     display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content: space-evenly;    
+    justify-content: space-between;      
 `;
 
-export const Li = styled.li`
-    display: inline;
-    font-size: 1.5rem;
-    font-family: sans-serif;
-    color: white;
+export const whiteDiv = styled.div`
+    width: 100%;
+    height: 500px;    
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;      
 `;
 
-export const Img = styled.img`
-    width: 100px;
-    height: 100px;
-`;
-
-export const H1 = styled.h1`
-    margin: 0;
-    font-size: 80px;
-    font-weight: bold;
-    font-family: sans-serif;
-    color: white;
-`;
-
-export const largerP = styled.p`
-    margin: 0;
-    font-size: 25px;
-    font-family: sans-serif;
-    color: white;
-`;
-
-export const smallerP = styled.p`
-    margin: 0;
-    font-size: 20px;
-    font-family: sans-serif;
-    color: white;
-`;
-
-export const H2 = styled.h2`
-    font-size: 50px;
-    font-weight: bold;
-    font-family: sans-serif;
-    color: white;
-`;
-
-export const mainButton = styled.button`
-
+export const divInfo = styled.div`
+    width: 500px;
+    height: 235px;    
+    padding-left: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-evenly;      
 `;
