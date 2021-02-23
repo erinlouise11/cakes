296b022430adc6a07416ed94fd4aed1dad3d4ebc
Erin Murphy <erinlouise11@gmail.com>
2021-02-23 13:56:03 -0500

    Media queries for text and images

--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const pinkButton = styled.button`
     width: auto;
     height: 40px;
-    margin-top: 20px;
+    margin-top: 10px;
     padding: 5px 10px;
     background-color: #CC84BD;
     color: #222;
@@ -43,7 +43,7 @@ export const bigPinkButton = styled.button`
 export const darkButton = styled.button`
     width: auto;
     height: 40px;
-    margin-top: 20px;
+    margin-top: 10px;
     padding: 5px 10px;
     background-color: #222;
     color: #CC84BD;
@@ -62,71 +62,72 @@ export const darkButton = styled.button`
 
 export const H1 = styled.h1`
     margin: 0;
-    font-size: 80px;
+    font-size: 40px;
     font-weight: bold;
     font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
     letter-spacing: 2px;
     color: #222;
 
     @media (min-width: 600px) {
-        
+        font-size: 60px;
     }
 
     @media (min-width: 1000px) {
-        
+        font-size: 80px;
     }
 `;
 
 export const largerP = styled.p`
     margin: 0;
-    font-size: 22px;
+    font-size: 16px;
+    font-weight: bold;
     font-family: 'Segoe Print', Tahoma, Geneva, Verdana, sans-serif;
     color: #222;
 
     @media (min-width: 600px) {
-        
+        font-size: 19px;
     }
 
     @media (min-width: 1000px) {
-        
+        font-size: 22px;
     }
 `;
 
 export const smallerP = styled.p`
     width: 450px;
     margin: 0;
-    font-size: 16px;
+    font-size: 14px;
     font-family: 'Segoe Print', Tahoma, Geneva, Verdana, sans-serif;
     color: #222;
 
     @media (min-width: 600px) {
-        
+        font-size: 16px;
     }
 
     @media (min-width: 1000px) {
-        
+        font-size: 17px;
     }
 `;
 
 export const H2 = styled.h2`
     margin: 0;
-    font-size: 45px;
+    font-size: 30px;
     font-weight: bold;
     font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
     letter-spacing: 2px;
     color: #222;
 
     @media (min-width: 600px) {
-        
+        font-size: 35px;   
     }
 
     @media (min-width: 1000px) {
-        
+        font-size: 45px;
     }
 `;
 
 export const Ul = styled.ul`
-    width: 900px;
+    width: 300px;
     height: 110px;
     padding-left: 0;
     margin: 0;
@@ -137,26 +138,28 @@ export const Ul = styled.ul`
     justify-content: space-evenly;    
 
     @media (min-width: 600px) {
-        
+        width: 500px;
+        height: 110px;
     }
 
     @media (min-width: 1000px) {
-        
+        width: 900px;
+        height: 110px;
     }
 `;
 
 export const Li = styled.li`
     display: inline;
-    font-size: 22px;
+    font-size: 16px;
     font-family: 'Segoe Print', Tahoma, Geneva, Verdana, sans-serif;
     color: #222;
 
     @media (min-width: 600px) {
-        
+        font-size: 19px;
     }
 
     @media (min-width: 1000px) {
-        
+        font-size: 22px;
     }
 `;
 
@@ -166,48 +169,54 @@ export const Logo = styled.img`
 `;
 
 export const divImg = styled.img`
-    width: 1000px;
-    height: 500px;    
+    width: 190px;
+    height: 320px;    
     object-fit: cover;
     object-position: top;
 
     @media (min-width: 600px) {
-        
+        width: 385px;
+        height: 500px;
     }
 
     @media (min-width: 1000px) {
-        
+        width: 1000px;
+        height: 500px;
     }
 `;
 
 export const sideImg = styled.img`
-    width: 270px;
-    height: 360px;    
+    width: 110px;
+    height: 140px;    
     object-fit: cover;
     object-position: bottom;
 
     @media (min-width: 600px) {
-        
+        width: 170px;
+        height: 200px;
     }
 
     @media (min-width: 1000px) {
-        
+        width: 270px;
+        height: 360px;
     }
 `;
 
 export const midImg = styled.img`
-    width: 340px;
-    height: 440px;    
+    width: 120px;
+    height: 180px;    
     object-fit: cover;
     object-position: center;
     border: solid 5px #CC84BD;
 
     @media (min-width: 600px) {
-        
+        width: 180px;
+        height: 240px;
     }
 
     @media (min-width: 1000px) {
-        
+        width: 340px;
+        height: 440px;
     }
 `;
 
@@ -228,20 +237,12 @@ export const HeroDivBig = styled.div`
         width: 100%;
         height: 100%;
     }
-
-    @media (min-width: 600px) {
-        
-    }
-
-    @media (min-width: 1000px) {
-        
-    }
 `;
 
 export const HeroDivSmall = styled.div`
     & {
         width: 100%;
-        height: 500px;
+        height: 280px;
         position: relative;
         top: 0;
         margin: 0;
@@ -257,11 +258,11 @@ export const HeroDivSmall = styled.div`
     }
 
     @media (min-width: 600px) {
-        
+        height: 450px;
     }
 
     @media (min-width: 1000px) {
-        
+        height: 500px; 
     }
 `;
 
@@ -277,60 +278,72 @@ export const Nav = styled.nav`
 `;
 
 export const homeHeroFlex = styled.div`
-    width: 600px;
-    height: 260px;
+    width: auto;
+    height: 200px;
     position: absolute;
     z-index: 1;
-    bottom: 20%;
-    left: 150px;
+    bottom: 3%;
+    left: 25px;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: space-evenly;     
     
     @media (min-width: 600px) {
-        
+        height: 270px;
+        bottom: 20%;
+        left: 150px;
     }
 
     @media (min-width: 1000px) {
-        
+        height: 270px;
+        bottom: 20%;
+        left: 150px;
     }
 `;
 
 export const heroTitleFlex = styled.div`
-    width: 400px;
+    width: 200px;
     height: 100px;
     position: absolute;
     z-index: 1;
-    bottom: 40%;
-    right: 300px;
+    bottom: 10%;
+    right: 60px;
     display: flex;
     flex-direction: column;
     align-items: flex-end;
 
     @media (min-width: 600px) {
-        
+        width: 400px;
+        height: 100px;
+        bottom: 40%;
+        right: 300px;
     }
 
     @media (min-width: 1000px) {
-        
+        width: 400px;
+        height: 100px;
+        bottom: 40%;
+        right: 300px;
     }
 `;
 
 export const buttonDiv = styled.div` 
-    width: 350px;
-    height: 60px;
+    width: 280px;
+    height: 40px;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between; 
     
     @media (min-width: 600px) {
-        
+        width: 350px;
+        height: 60px;
     }
 
     @media (min-width: 1000px) {
-        
+        width: 350px;
+        height: 60px;
     }
 `;
 
